c537c8492ea5b14ce1aa489ee2d2acfd2d385647
Alexandra 207 <alexkuchynsk@gmail.com>
2020-10-21 19:45:21 +0300

    Домашнее задание: 3.2 и 4.1

--- js/main.js
+++ js/main.js
@@ -47,13 +47,72 @@ const PIN_OFFSET_Y = 84;
 const PIN_OFFSET_X = 31;
 
 const mapDialog = document.querySelector(`.map`);
-// mapDialog.classList.remove(`map--faded`);
 
 const announcementPins = document.querySelector(`.map__pins`);
 const advertTask = document.querySelector(`#pin`).content;
 const advertTemplate = advertTask.querySelector(`.map__pin`);
 
 const fragment = document.createDocumentFragment();
+
+const cardTask = document.querySelector(`#card`).content;
+const cardTemplate = cardTask.querySelector(`.popup`);
+const appartmentTypeTranslation = {
+  flat: `Квартира`,
+  bungalow: `Бунгало`,
+  house: `Дом`,
+  palace: `Дворец`
+};
+
+const mapFiltersContainer = document.querySelector(`.map__filters-container`);
+
+const elementsFieldset = document.querySelectorAll(`.ad-form__element`);
+const form = document.querySelector(`.ad-form`);
+const titleInput = form.querySelector(`#title`);
+const mapFilters = document.querySelector(`.map__filters`);
+const MIN_TITLE_LENGTH = 30;
+const MAX_TITLE_LENGTH = 100;
+
+const pinMain = document.querySelector(`.map__pin--main`);
+const addressInput = document.querySelector(`#address`);
+
+const priceInput = form.querySelector(`#price`);
+const typeField = form.querySelector(`#type`);
+const MAX_INPUT_PRICE = 1000000;
+
+let houseTypes = {
+  'palace': {
+    min: 10000,
+    placeholder: `10000`
+  },
+  'flat': {
+    min: 1000,
+    placeholder: `1000`
+  },
+  'house': {
+    min: 5000,
+    placeholder: `5000`
+  },
+  'bungalow': {
+    min: 0,
+    placeholder: `0`
+  },
+};
+
+const pinMainActiveCoordinateX = 570 + 33;
+const pinMainActiveCoordinateY = 375 + 33;
+const pinMaininActiveCoordinateX = 570;
+const pinMaininActiveCoordinateY = 375;
+
+const roomNumberInput = form.querySelector(`#room_number`);
+const capacityInput = form.querySelector(`#capacity`);
+const VALUE_0 = `0`;
+const VALUE_1 = `1`;
+const VALUE_2 = `2`;
+const VALUE_3 = `3`;
+const VALUE_100 = `100`;
+
+const timeIn = form.querySelector(`#timein`);
+const timeOut = form.querySelector(`#timeout`);
 
 function getRandomIntInclusive(min, max) {
   let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
@@ -63,8 +122,7 @@ function getRandomIntInclusive(min, max) {
 let getAvatar = function () {
   let avatarArr = [];
   for (let i = 1; i <= 8; i++) {
-    let newAvatar = `img/avatars/user` + `0` + i + `.png`;
-    avatarArr.push(newAvatar);
+    avatarArr.push(`img/avatars/user0${i}.png`);
   }
   return avatarArr;
 };
@@ -75,54 +133,72 @@ let getRandomLength = function (arr) {
 };
 
 const getRandomParameter = function (arr) {
-  let randomParameter = arr[Math.floor(Math.random() * arr.length)];
-  return randomParameter;
+  return arr[Math.floor(Math.random() * arr.length)];
 };
 
 const getObjectArray = function (arr) {
   let someArray = [];
-  for (let i = 0; i < getRandomLength(arr.length); i++) {
+  for (let i = 0; i < getRandomLength(arr); i++) {
     someArray.push(getRandomParameter(arr));
   }
   return someArray;
 };
+// let getRandomLength = function (max) {
+  // return Math.floor(Math.random() * max);
+// };
 
+// const getRandomParameter = function (max) {
+//  return [Math.floor(Math.random() * max)];
+// };
+
+// const getObjectArray = function (max) {
+//  let someArray = [];
+//  for (let i = 0; i < getRandomLength(max); i++) {
+//  someArray.push(getRandomParameter(max));
+//  }
+//  return someArray;
+// };
 let getArray = function () {
   const announcementArray = [];
   for (let i = 0; i < 8; i++) {
 
-    const randomAvatar = getRandomParameter(avatarArray);
-    const randomTitle = getRandomParameter(ADVERT_TITLE);
-    const randomCheckin = getRandomParameter(CHECKIN);
-    const randomCheckout = getRandomParameter(CHECKOUT);
-    const randomType = getRandomParameter(ADVERT_TYPE);
-    const randomDescription = getRandomParameter(ADVERT_DESCRIPTION);
-    const locationX = getRandomIntInclusive(MIN_X, MAX_X);
-    const locationY = getRandomIntInclusive(MIN_Y, MAX_Y);
-    const randomPrice = getRandomIntInclusive(MIN_PRICE, MAX_PRICE);
-    const randomRooms = getRandomIntInclusive(MIN_ROOMS, MAX_ROOMS);
-    const randomGuests = getRandomIntInclusive(MIN_GUESTS, MAX_GUESTS);
-    const randomFeatureArray = getObjectArray(FEATURES);
-    const randomPhotosArray = getObjectArray(PHOTOS);
-
     announcementArray.push({
-      avatar: randomAvatar,
-      title: randomTitle,
-      address: `${locationX}, ${locationY}`,
-      price: randomPrice,
-      type: randomType,
-      rooms: randomRooms,
-      guests: randomGuests,
-      checkin: randomCheckin,
-      checkout: randomCheckout,
-      features: randomFeatureArray,
-      description: randomDescription,
-      photos: randomPhotosArray,
+      avatar: getRandomParameter(avatarArray),
+      title: getRandomParameter(ADVERT_TITLE),
+      address: `${getRandomIntInclusive(MIN_X, MAX_X)}, ${getRandomIntInclusive(MIN_Y, MAX_Y)}`,
+      price: getRandomIntInclusive(MIN_PRICE, MAX_PRICE),
+      type: getRandomParameter(ADVERT_TYPE.length),
+      rooms: getRandomIntInclusive(MIN_ROOMS, MAX_ROOMS),
+      guests: getRandomIntInclusive(MIN_GUESTS, MAX_GUESTS),
+      checkin: getRandomParameter(CHECKIN),
+      checkout: getRandomParameter(CHECKOUT),
+      features: getObjectArray(FEATURES),
+      description: getRandomParameter(ADVERT_DESCRIPTION),
+      photos: getObjectArray(PHOTOS),
       location: {
-        y: locationY,
-        x: locationX,
+        y: getRandomIntInclusive(MIN_Y, MAX_Y),
+        x: getRandomIntInclusive(MIN_X, MAX_X),
       }
     });
+    // announcementArray.push({
+    //  avatar: getRandomParameter(avatarArray.length),
+    //  title: getRandomParameter(ADVERT_TITLE.length),
+    //  address: `${getRandomIntInclusive(MIN_X, MAX_X)}, ${getRandomIntInclusive(MIN_Y, MAX_Y)}`,
+    //  price: getRandomIntInclusive(MIN_PRICE, MAX_PRICE),
+    //  type: getRandomParameter(ADVERT_TYPE.length),
+    //  rooms: getRandomIntInclusive(MIN_ROOMS, MAX_ROOMS),
+    //  guests: getRandomIntInclusive(MIN_GUESTS, MAX_GUESTS),
+    //  checkin: getRandomParameter(CHECKIN.length),
+    //  checkout: getRandomParameter(CHECKOUT.length),
+    //  features: getObjectArray(FEATURES),
+    //  description: getRandomParameter(ADVERT_DESCRIPTION.length),
+    //  photos: getObjectArray(PHOTOS),
+    //  location: {
+    //    y: getRandomIntInclusive(MIN_Y, MAX_Y),
+    //    x: getRandomIntInclusive(MIN_X, MAX_X),
+    //  }
+    // });
+
   }
   return announcementArray;
 };
@@ -148,46 +224,73 @@ const renderFragment = function () {
 };
 
 // 3.2 Личный проект: больше деталей (часть 2)
-const cardTask = document.querySelector(`#card`).content;
-const cardTemplate = cardTask.querySelector(`.popup`);
-const appartmentTypeTranslation = {
-  flat: `Квартира`,
-  bungalow: `Бунгало`,
-  house: `Дом`,
-  palace: `Дворец`
-};
-
-const mapFiltersContainer = document.querySelector(`.map__filters-container`);
-
 let renderPopup = function (information) {
   let popupElement = cardTemplate.cloneNode(true);
   popupElement.querySelector(`.popup__title`).textContent = information.title;
   popupElement.querySelector(`.popup__text--address`).textContent = information.address;
-  popupElement.querySelector(`.popup__text--price`).textContent = information.price + `₽/ночь`;
+  popupElement.querySelector(`.popup__text--price`).textContent = `${information.price} ₽/ночь`;
   popupElement.querySelector(`.popup__type`).textContent = appartmentTypeTranslation[information.type];
   popupElement.querySelector(`.popup__text--capacity`).textContent = `${information.rooms} комнаты для ${information.guests} гостей`;
   popupElement.querySelector(`.popup__text--time`).textContent = `Заезд после ${information.checkin}, выезд до ${information.checkout}`;
-  // Не получается здесь. Если я делаю так,
-  // то на странице отображается весь список удобств (как в разметке),
-  // когда на самом деле из этого списка  в случайно сгенерированном только 2 удобства.
-  // Думаю, что у меня не получается записать правильно каждое удобство в отлельный тег li  с соответствующим классом
-  popupElement.querySelector(`.popup__feature`).length = information.features.length;
-  popupElement.querySelector(`.popup__description`).textContent = information.description;
-  popupElement.querySelector(`.popup__avatar`).src = information.avatar;
-  // Здесь похожая проблема, у меня  не получается записать каждую строку массива фотографий в отдельный тег img.
-  // Пробовала так, но тегов img больше не становится, а src = unknown.
 
-  // popupElement.querySelector(`.popup__photos`).src = information.photos;
-
-  const photoPopup = popupElement.querySelector(`.popup__photo`);
-  // let length = Math.min(getObjectArray(PHOTOS).length, popupElement.length);
-
-  for (let i = 0; i < getObjectArray(PHOTOS).length; i++) {
-    // popupElement.querySelector(`.popup__photos`).appendChild(photoPopup[i]);
-    photoPopup.length = information.photos.length;
-    photoPopup[i].src = information.photos[i];
+  const featureElements = popupElement.querySelectorAll(`.popup__feature`);
+  for (let i = 0; i < featureElements.length; i++) {
+    const featureElement = featureElements[i];
+    const checkFeatures = function (feature) {
+      return featureElement.classList.contains(`popup__feature--${feature}`);
+    };
+    const featureIsIncluded = information.features.some(checkFeatures);
+    if (!featureIsIncluded) {
+      featureElement.classList.add(`visually-hidden`);
+    }
   }
 
+  popupElement.querySelector(`.popup__description`).textContent = information.description;
+  popupElement.querySelector(`.popup__avatar`).src = information.avatar;
+
+  const popupPhotoContainer = popupElement.querySelector(`.popup__photos`);
+  const photoPopup = popupElement.querySelector(`.popup__photo`);
+  const fragmentPhoto = document.createDocumentFragment();
+
+  information.photos.forEach((element) => {
+    let photoElement = photoPopup.cloneNode(true);
+    photoElement.src = element;
+    photoElement.style.width = `45`;
+    photoElement.style.height = `40`;
+    photoElement.alt = `Фотография жилья`;
+    fragmentPhoto.appendChild(photoElement);
+  });
+
+  popupPhotoContainer.innerHTML = ``;
+  popupPhotoContainer.appendChild(fragmentPhoto);
+
+  if (information.title === undefined) {
+    popupElement.querySelector(`.popup__title`).classList.add(`visually-hidden`);
+  }
+  if (information.address === undefined) {
+    popupElement.querySelector(`.popup__text--address`).classList.add(`visually-hidden`);
+  }
+  if (information.price === undefined) {
+    popupElement.querySelector(`.popup__text--price`).classList.add(`visually-hidden`);
+  }
+  if (information.type === undefined) {
+    popupElement.querySelector(`.popup__type`).classList.add(`visually-hidden`);
+  }
+  if (information.rooms === undefined || information.guests === undefined) {
+    popupElement.querySelector(`.popup__text--capacity`).classList.add(`visually-hidden`);
+  }
+  if (information.checkin === undefined || information.checkout === undefined) {
+    popupElement.querySelector(`.popup__text--capacity`).classList.add(`visually-hidden`);
+  }
+  if (information.description === undefined) {
+    popupElement.querySelector(`.popup__description`).classList.add(`visually-hidden`);
+  }
+  if (information.avatar === undefined) {
+    popupElement.querySelector(`.popup__avatar`).classList.add(`visually-hidden`);
+  }
+  if (information.photos.length === undefined) {
+    popupPhotoContainer.classList.add(`visually-hidden`);
+  }
   return popupElement;
 };
 
@@ -199,23 +302,9 @@ const renderPopupFragment = function () {
 };
 
 // 4.1 задание
-const elementsFieldset = document.querySelectorAll(`.ad-form__element`);
-const form = document.querySelector(`.ad-form`);
-const titleInput = form.querySelector(`#title`);
-const mapFilters = document.querySelector(`.map__filters`);
-const MIN_TITLE_LENGTH = 30;
-const MAX_TITLE_LENGTH = 100;
 for (let elementFieldset of elementsFieldset) {
   elementFieldset.setAttribute(`disabled`, `true`);
 }
-
-const pinMain = document.querySelector(`.map__pin--main`);
-let addressInput = document.querySelector(`#address`);
-
-const pinMainActiveCoordinateX = 570 + 33;
-const pinMainActiveCoordinateY = 375 + 33;
-const pinMaininActiveCoordinateX = 570;
-const pinMaininActiveCoordinateY = 375;
 
 addressInput.value = `${pinMaininActiveCoordinateX}, ${pinMaininActiveCoordinateY}`;
 
@@ -224,8 +313,8 @@ let onPinMainMousedown = function () {
     elementFieldset.removeAttribute(`disabled`);
   }
   mapDialog.classList.remove(`map--faded`);
-  form.classList.remove(`.ad-form--disabled`);
-  mapFilters.classList.remove(`.map__filters--disabled`);
+  form.classList.remove(`ad-form--disabled`);
+  mapFilters.classList.remove(`map__filters--disabled`);
   addressInput.value = `${pinMainActiveCoordinateX}, ${pinMainActiveCoordinateY}`;
   renderFragment();
   renderPopupFragment();
@@ -247,57 +336,35 @@ pinMain.addEventListener(`keydown`, function (evt) {
 titleInput.addEventListener(`input`, function () {
   const valueLength = titleInput.value.length;
   if (valueLength < MIN_TITLE_LENGTH) {
-    titleInput.setCustomValidity(`Еще ` + (MIN_TITLE_LENGTH - valueLength) + ` символов`);
+    titleInput.setCustomValidity(`Еще ${MIN_TITLE_LENGTH - valueLength} символов`);
+    titleInput.classList.add(`wrong-input`);
   } else if (valueLength > MAX_TITLE_LENGTH) {
-    titleInput.setCustomValidity(`Удалите лишние ` + (valueLength - MAX_TITLE_LENGTH) + ` симв.`);
+    titleInput.setCustomValidity(`Удалите лишние ${valueLength - MAX_TITLE_LENGTH} симв.`);
   } else {
     titleInput.setCustomValidity(``);
   }
   titleInput.reportValidity();
 });
 
-
-const roomNumberInput = form.querySelector(`#room_number`);
-const capacityInput = form.querySelector(`#capacity`);
-
-form.addEventListener(`change`, function () {
-  if (capacityInput.value === `1` && roomNumberInput.value !== `1`) {
-    roomNumberInput.setCustomValidity(`Для 1 гостя может быть доступна только ${roomNumberInput.value = `1`} комната`);
-  } else if (capacityInput.value === `2` && roomNumberInput.value !== `2` && roomNumberInput.value !== `1`) {
-    roomNumberInput.setCustomValidity(`Для 2 гостей может быть доступно от ${roomNumberInput.value = `1`} до ${roomNumberInput.value = `2`} комнат`);
-  } else if (capacityInput.value === `3` && roomNumberInput.value !== `3` && roomNumberInput.value !== `2` && roomNumberInput.value !== `1`) {
-    roomNumberInput.setCustomValidity(`Для 3 гостей может быть доступно от ${roomNumberInput.value = `1`} до ${roomNumberInput.value = `3`} комнат`);
-  } else if (capacityInput.value === `0` && roomNumberInput.value !== `100`) {
-    roomNumberInput.setCustomValidity(`При выборе опции "не для гостей" может быть доступно только ${roomNumberInput.value = `100`}  комнат`);
+const onroomNumberChange = function () {
+  if (capacityInput.value === VALUE_1 && roomNumberInput.value !== VALUE_1) {
+    roomNumberInput.setCustomValidity(`Для 1 гостя может быть доступна только ${roomNumberInput.value = VALUE_1} комната`);
+  } else if (capacityInput.value === VALUE_2 && roomNumberInput.value !== VALUE_2 && roomNumberInput.value !== VALUE_1) {
+    roomNumberInput.setCustomValidity(`Для 2 гостей может быть доступно от ${roomNumberInput.value = VALUE_1} до ${roomNumberInput.value = VALUE_2} комнат`);
+  } else if (capacityInput.value === VALUE_3 && roomNumberInput.value !== VALUE_3 && roomNumberInput.value !== VALUE_2 && roomNumberInput.value !== VALUE_1) {
+    roomNumberInput.setCustomValidity(`Для 3 гостей может быть доступно от ${roomNumberInput.value = VALUE_1} до ${roomNumberInput.value = VALUE_3} комнат`);
+  } else if (capacityInput.value === VALUE_0 && roomNumberInput.value !== VALUE_100) {
+    roomNumberInput.setCustomValidity(`При выборе опции "не для гостей" может быть доступно только ${roomNumberInput.value = VALUE_100}  комнат`);
   } else {
     roomNumberInput.setCustomValidity(``);
   }
   roomNumberInput.reportValidity();
+};
+
+form.addEventListener(`change`, function () {
+  onroomNumberChange();
 });
 
-
-const priceInput = form.querySelector(`#price`);
-const typeField = form.querySelector(`#type`);
-const MAX_INPUT_PRICE = 1000000;
-
-let houseTypes = {
-  'palace': {
-    min: 10000,
-    placeholder: `10000`
-  },
-  'flat': {
-    min: 1000,
-    placeholder: `1000`
-  },
-  'house': {
-    min: 5000,
-    placeholder: `5000`
-  },
-  'bungalow': {
-    min: 0,
-    placeholder: `0`
-  },
-};
 
 let onTypeFieldChange = function () {
   priceInput.placeholder = houseTypes[typeField.value].placeholder;
@@ -310,20 +377,17 @@ let onTypeFieldChange = function () {
 typeField.addEventListener(`change`, function () {
   onTypeFieldChange();
 });
-// Также хотела установить setCustomValidity параллельно не только для минимального значения,
-// но и для максимального значения цены. В итоге пропадает как «первое сообщение», так и второе
-// let onPriceInputChange = function () {
-  // const maxCost = priceInput.value.length > MAX_INPUT_PRICE ? `Максимальная стоимость должна составлять ${MAX_PRICE}` : ``;
-  // priceInput.setCustomValidity(maxCost);
-  // priceInput.reportValidity();
-// };
 
-// form.addEventListener(`change`, function () {
-  // onPriceInputChange();
-// });
+let onPriceInputChange = function () {
+  const maxCost = parseInt(priceInput.value, 10) > MAX_INPUT_PRICE ? `Максимальная стоимость должна составлять ${MAX_INPUT_PRICE}` : ``;
+  priceInput.setCustomValidity(maxCost);
+  priceInput.reportValidity();
+};
 
-const timeIn = form.querySelector(`#timein`);
-const timeOut = form.querySelector(`#timeout`);
+priceInput.addEventListener(`input`, function () {
+  onPriceInputChange();
+});
+
 let onTimeInChange = function () {
   timeOut.value = timeIn.value;
 };
@@ -333,6 +397,39 @@ timeIn.addEventListener(`change`, function () {
 });
 
 
-const submitButton = document.querySelector(`.ad-form__submit`);
-submitButton.addEventListener(`submit`, function () {
+// const submitButton = document.querySelector(`.ad-form__submit`);
+// const successTask = document.querySelector(`#success`).content;
+// const successTemplate = successTask.querySelector(`.success`);
+// const main = document.querySelector(`.main`);
+
+// 2.8. Нажатие на кнопку .ad-form__reset сбрасывает страницу в исходное неактивное состояние без перезагрузки, а также:
+// •	все заполненные поля возвращаются в изначальное состояние, в том числе фильтры;
+// •	метки похожих объявлений и карточка активного объявления удаляются;
+// •	метка адреса возвращается в исходное положение;
+// •	значение поля адреса корректируется соответственно положению метки;
+
+// ВОПРОС по 2.8 Не получается сделать так, чтобы все заполненные поля возвращались в изначальное состояние (обнулялись), кроме значение поля адреса, которое корректируется соответственно положению метки
+// Для этого обнуляю все поля, кроме поля с id = 'address' и добавляю в слушатель событтий evt.preventDefault(), но поля не становятся пустыми
+
+const allInputsForm = form.querySelectorAll(`input`);
+let cleanInputs = function () {
+  for (let i = 0; i < allInputsForm.length; i++) {
+    const inputElement = allInputsForm[i];
+    if (inputElement.id !== `address`) {
+      inputElement.value = ``;
+    }
+  }
+};
+
+let pageInactivemake = function () {
+  mapDialog.classList.add(`map--faded`);
+  addressInput.value = `${pinMaininActiveCoordinateX}, ${pinMaininActiveCoordinateY}`;
+  announcementPins.innerHTML = ``;
+};
+const resetButton = document.querySelector(`.ad-form__reset`);
+
+resetButton.addEventListener(`click`, function (evt) {
+  evt.preventDefault();
+  cleanInputs();
+  pageInactivemake();
 });
