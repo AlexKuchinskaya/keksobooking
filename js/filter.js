c061520ae00516d1e4645d65fd11b8f5afd7916f
Alexandra 207 <alexkuchynsk@gmail.com>
2020-11-12 17:22:50 +0300

    Выполняет 7.2

--- js/filter.js
+++ js/filter.js
@@ -3,34 +3,35 @@
   const PRICE_10000 = 10000;
   const PRICE_50000 = 50000;
   const ANY_VALUE = `any`;
+  const LOW_PRICE = `low`;
+  const MIDDLE_PRICE = `middle`;
+  const HIGH_PRICE = `high`;
   const ROOM_1 = 1;
   const ROOMS_2 = 2;
+  const ROOM_1_STRING = `1`;
+  const ROOM_2_STRING = `2`;
+  const ROOM_3_STRING = `3`;
   const GUEST_1 = 1;
+  const GUEST_1_STRING = `1`;
+  const GUEST_2_STRING = `2`;
+  const GUEST_0_STRING = `0`;
   const GUESTS_2 = 2;
+  const MAX_PINS = 5;
   const mapFilters = document.querySelector(`.map__filters`);
   const typeHouseFilter = mapFilters.querySelector(`#housing-type`);
   const housingPriceFilter = mapFilters.querySelector(`#housing-price`);
   const housingRoomsFilter = mapFilters.querySelector(`#housing-rooms`);
   const housingGuestsFilter = mapFilters.querySelector(`#housing-guests`);
   const housingFeaturesFilter = mapFilters.querySelector(`#housing-features`);
+  const elementsFilterForm = mapFilters.children;
 
-  if (window.form.getPinsData() === `undefined`) {
-    mapFilters.children.disabled = true;
-  }
-  const getHousingTypeChangeValue = () => {
-    return typeHouseFilter.value;
-  };
-  const getHousingPriceChangeValue = () => {
-    return housingPriceFilter.value;
+  const checkMapPinsUndefined = () => {
+    if (window.form.getPinsData() === `undefined`) {
+      mapFilters.children.disabled = true;
+    }
   };
 
-  const getHousingRoomsChangeValue = () => {
-    return housingRoomsFilter.value;
-  };
-
-  const getHousingGuestsChangeValue = () => {
-    return housingGuestsFilter.value;
-  };
+  checkMapPinsUndefined();
 
   const getCheckedCheckboxes = () => {
     const checkboxes = housingFeaturesFilter.querySelectorAll(`input`);
@@ -40,99 +41,95 @@
         checkboxesChecked.push(checkboxes[i].value);
       }
     }
-    // return checkboxesChecked.length > 0 ? checkboxesChecked : null;
     return checkboxesChecked;
   };
 
-  const filterPinsByHousingFeatures = (pins, featuresArray) => {
-    return pins.filter((pin) => {
-      return featuresArray.every((feature) => {
-        return pin.offer.features.includes(feature);
-      });
-    });
+  const filterPinsByHousingFeatures = (pin) => {
+    const featuresArray = getCheckedCheckboxes();
+    return featuresArray.every((feature) => {
+      return pin.offer.features.includes(feature);
+    }) ? pin : null;
   };
-  const filterPinsByHousingType = (pins, housingType) => {
+
+  const filterPinsByHousingType = (pin) => {
+    const housingType = typeHouseFilter.value;
     if (housingType === ANY_VALUE) {
-      return pins;
-    } else {
-      return pins.filter((pin) => {
-        return pin.offer.type === housingType;
-      });
+      return pin;
     }
+    return pin.offer.type === housingType ? pin : null;
   };
+
   const getPriceCategoryFromNumber = (priceNumber) => {
     if (priceNumber < PRICE_10000) {
-      return `low`;
+      return LOW_PRICE;
     }
     if (priceNumber > PRICE_50000) {
-      return `high`;
+      return HIGH_PRICE;
     }
-    return `middle`;
+    return MIDDLE_PRICE;
   };
-  const filterPinsByHousingPrice = (pins, priceCatagory) => {
-    if (priceCatagory === ANY_VALUE) {
-      return pins;
-    } else {
-      return pins.filter((pin) => {
-        return getPriceCategoryFromNumber(pin.offer.price) === priceCatagory;
-      });
+
+  const filterPinsByHousingPrice = (pin) => {
+    const priceCategory = housingPriceFilter.value;
+    if (priceCategory === ANY_VALUE) {
+      return pin;
     }
+    return getPriceCategoryFromNumber(pin.offer.price) === priceCategory ? pin : null;
   };
+
   const getRoomСategory = (roomNumber) => {
     if (roomNumber === ROOM_1) {
-      return `1`;
+      return ROOM_1_STRING;
     }
     if (roomNumber === ROOMS_2) {
-      return `2`;
+      return ROOM_2_STRING;
     }
-    return `3`;
+    return ROOM_3_STRING;
   };
-  const filterPinsByHousingRooms = (pins, roomCategory) => {
+
+  const filterPinsByHousingRooms = (pin) => {
+    const roomCategory = housingRoomsFilter.value;
     if (roomCategory === ANY_VALUE) {
-      return pins;
-    } else {
-      return pins.filter((pin) => {
-        return getRoomСategory(pin.offer.rooms) === roomCategory;
-      });
+      return pin;
     }
+    return getRoomСategory(pin.offer.rooms) === roomCategory ? pin : null;
   };
+
   const getGuestCategory = (guestNumber) => {
     if (guestNumber === GUEST_1) {
-      return `1`;
+      return GUEST_1_STRING;
     }
     if (guestNumber === GUESTS_2) {
-      return `2`;
+      return GUEST_2_STRING;
     }
-    return `0`;
+    return GUEST_0_STRING;
   };
-  const filterPinsByHousingGuests = (pins, guestCategory) => {
+
+  const filterPinsByHousingGuests = (pin) => {
+    const guestCategory = housingGuestsFilter.value;
     if (guestCategory === `any`) {
-      return pins;
-    } else {
-      return pins.filter((pin) => {
-        return getGuestCategory(pin.offer.guests) === guestCategory;
-      });
+      return pin;
     }
+    return getGuestCategory(pin.offer.guests) === guestCategory ? pin : null;
   };
-  const elementsFilterForm = mapFilters.children;
+
   for (let i = 0; i < elementsFilterForm.length; i++) {
     let filterElement = elementsFilterForm[i];
     filterElement.addEventListener(`change`, () => {
-      const selectedHousingType = getHousingTypeChangeValue();
-      const selectedHousingPrice = getHousingPriceChangeValue();
-      const selectedHousingRooms = getHousingRoomsChangeValue();
-      const selectedHousingGuests = getHousingGuestsChangeValue();
-      const selectedFeatures = getCheckedCheckboxes();
       const allPins = window.form.getPinsData();
-      const pinsFilteredByHousingType = filterPinsByHousingType(allPins, selectedHousingType);
-      const pinsFilteredByPrice = filterPinsByHousingPrice(pinsFilteredByHousingType, selectedHousingPrice);
-      const pinsFilteredByRooms = filterPinsByHousingRooms(pinsFilteredByPrice, selectedHousingRooms);
-      const pinsFilteredByGuests = filterPinsByHousingGuests(pinsFilteredByRooms, selectedHousingGuests);
-      const pinsFilteredByAll = filterPinsByHousingFeatures(pinsFilteredByGuests, selectedFeatures);
-      window.pin.renderPins(pinsFilteredByAll);
-      if (document.querySelector(`.popup`)) {
-        window.card.closeAllPopups();
+      const filters = [filterPinsByHousingType, filterPinsByHousingPrice, filterPinsByHousingRooms, filterPinsByHousingGuests, filterPinsByHousingFeatures];
+      const filteredPins = [];
+      for (let pinIndex = 0; pinIndex < allPins.length && filteredPins.length < MAX_PINS; pinIndex++) {
+        let pin = allPins[pinIndex];
+        for (let filterIndex = 0; filterIndex < filters.length && pin; filterIndex++) {
+          pin = filters[filterIndex](pin);
+        }
+        if (pin) {
+          filteredPins.push(pin);
+        }
       }
+      window.pin.renderPins(filteredPins);
+      window.card.closeAllPopups();
     });
   }
 })();
